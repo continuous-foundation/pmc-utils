30c486f98b6bfcc5ef23c0d5d6d06cd4e5870c7e
Steve Purves <steve@curvenote.com>
2026-02-05 15:40:28 +0000

    🧹 cleaning out cloud services ready for transfer

--- packages/ftp-service/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { createService } from './service.js';
-
-const service = createService();
-
-const port = process.env.PORT || 8080;
-service.listen(port, () => {
-  console.info(`pmc-ftp-server: listening on port ${port}`);
-});
